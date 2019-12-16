d7b5432d4284146715b135872a6d97d12cea2143
Hanno Brandwijk <brandwijkh@gmail.com>
2019-12-16 10:16:30 +0100

    MainActivity Cleanup

--- app/src/main/java/com/b/gpshelperbreda/activity/MainActivity.java
+++ app/src/main/java/com/b/gpshelperbreda/activity/MainActivity.java
@@ -31,48 +31,10 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Database database;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        database = new Database(this);
     }
 
-    public void buttonOnClick(View view) {
-        database.insertValue(new Waypoint("1", 2, 1, 10, 10, "Eerste", "Dit is de eerste", true));
-    }
-
-    public void button2OnClick(View view) {
-        String tekst = "";
-        ArrayList<Waypoint> waypoints = database.readValues();
-
-
-        for (Waypoint w : waypoints) {
-            tekst += w.toString();
-            tekst += "\n";
-        }
-
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(tekst);
-
-    }
-
-    public void button3onClick(View view) {
-        database.dropTable();
-    }
-
-    public void button4onClick(View view) {
-        RouteFactory routeFactory = new RouteFactory(this);
-        Route route = routeFactory.getRouteFromId(1);
-
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(route.toString());
-    }
-
-    public void button5onClick(View view) {
-        new JsonParser(this).parseJson("waypoints.json");
-    }
 }
