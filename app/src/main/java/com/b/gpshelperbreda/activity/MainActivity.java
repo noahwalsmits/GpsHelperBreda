95fe7c2ab85f147aa21980dd7b85ba37503b634d
itsarshiya <47162385+itsarshiya@users.noreply.github.com>
2019-12-16 03:22:15 +0100

    added styling to map and main activity

--- app/src/main/java/com/b/gpshelperbreda/activity/MainActivity.java
+++ app/src/main/java/com/b/gpshelperbreda/activity/MainActivity.java
@@ -2,7 +2,10 @@ package com.b.gpshelperbreda.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.b.gpshelperbreda.R;
 
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Route button */
+    public void SelectRoute(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
